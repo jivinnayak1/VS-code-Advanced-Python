32e7c15ba11ea99e7ccf26c4779bb60831f2eb8c
Jivin Nayak <jivin.nayak@gmail.com>
2025-01-29 16:28:33 +0530

    Add files via upload

---
+++ Intro_to_Python/Tuple_ToDoList.py
@@ -0,0 +1,6 @@
+Personal_Details = ("Jivin Nayak","12","172cm","61kg","Design")
+
+Personal_DetailsList = list(Personal_Details)
+
+print("Tuple: ",Personal_Details)
+print("List: ",Personal_DetailsList)
