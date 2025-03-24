65dd296c044947148e401f3a785aaecd00070401
Jivin Nayak <jivin.nayak@gmail.com>
2025-03-24 20:30:50 +0530

    Add files via upload

---
+++ Bit_Play.py
@@ -0,0 +1,20 @@
+# a=5
+# b=6
+# print(a^b)
+
+a = 3
+b = 4
+
+# print(a+1)
+# print(b-1)
+# c = a #3
+# a = b #4
+# b = c #3
+# print(a)
+# print(b)
+
+a = a+b #7
+b = a-b #3
+a = a-b #4
+print(a)
+print(b)
